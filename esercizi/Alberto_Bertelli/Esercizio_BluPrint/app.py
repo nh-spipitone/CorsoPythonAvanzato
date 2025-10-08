b9234ad536352c5646b289e535c7bcb76f0ec606
ABertelli98 <albertobert98@yahoo.it>
2025-10-08 17:08:47 +0200

    aggiunte

--- esercizi/Alberto_Bertelli/Esercizio_BluPrint/app.py
+++ esercizi/Alberto_Bertelli/Esercizio_BluPrint/app.py
@@ -8,3 +8,15 @@ app.register_blueprint(tasks_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# app.py → avvia il server e collega i blueprint
+
+# routes.py → contiene le funzioni che rispondono alle richieste
+
+# jsonify() → serve per rispondere in formato JSON
+
+# global next_id → serve per aggiornare il contatore degli ID
+
+# tasks → è una lista che contiene tutti i task creati
