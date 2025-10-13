f56315162e3d7878f38d25ff67567deace928ce6
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2025-10-13 14:38:07 +0200

    simple example about payload types validation

--- esercizi/app/user.py
+++ esercizi/app/user.py
@@ -1,6 +1,7 @@
-from typing import Any, Literal
+from typing import Any
 
 from flask import Blueprint, abort, request
+from pydantic import BaseModel, PositiveInt
 
 
 bp = Blueprint("user", "user", url_prefix="/user")
@@ -16,18 +17,27 @@ def get_user(user_id: int) -> tuple[dict[str, Any], int]:
     return cache[user_id], 200
 
 
+class UserPayload(BaseModel):
+    username: str
+    eta: PositiveInt
+
+
 @bp.route("/", methods=("POST",))
 def post_user() -> tuple[dict[str, Any], int]:
     username = request.form.get("username")
     eta = request.form.get("eta")
-    if not username or not eta:
-        abort(400)
+    
+    try:
+        user = UserPayload(username=username, eta=eta)
+    except:
+        return abort(400)
 
+    global current_user_id
     current_user_id += 1
     obj: dict[str, Any] = {
         "user_id": current_user_id,
-        "username": username,
-        "eta": eta,
+        "username": user.username,
+        "eta": user.eta,
     }
     cache[current_user_id] = obj
     return obj, 201
