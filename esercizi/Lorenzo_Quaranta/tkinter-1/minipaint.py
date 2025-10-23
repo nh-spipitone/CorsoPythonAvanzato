8f2dfc99990ca05f126f60fea4c927072aef4d43
LQ95 <lorenzo.quaranta1995@libero.it>
2025-10-23 19:53:36 +0200

    Update minipaint.py

--- esercizi/Lorenzo_Quaranta/tkinter-1/minipaint.py
+++ esercizi/Lorenzo_Quaranta/tkinter-1/minipaint.py
@@ -10,9 +10,18 @@ class minipaint(tk.Tk):
         self.canvas.pack()
         self.stateLabel=tk.Label(self)
         self.stateLabel.pack()
+        self.paint_color="black"
     
-
+    def onclick(self):
+            self.canvas.create_oval(x-r, y-r, x+r, y+r, fill=self.paint_color, outline="")
+    
+    def change_col(self):
+        if self.paint_color=="black":
+            self.paint_color="blue" 
+        else: self.paint_color="black"
 
 
 app=minipaint()
+
+
 app.mainloop()
